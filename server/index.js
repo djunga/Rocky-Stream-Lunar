f9a6561291f303164ce3d9b71ef8cafe4e84abc4
ToraMullings316 <tora.mullings@stonybrook.edu>
2021-07-29 22:27:58 -0400

    Hide credentials

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
-const cors = require("cors");
+const cors = require("cors")
+const credentials = require('./credentials')
 
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'root',
-    database: 'lunar'
+    user: credentials['user'],
+    host: credentials['host'],
+    password: credentials['password'],
+    database: credentials['database']
 })
 
 // Verify if login info exists and is correct
